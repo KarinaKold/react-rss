7f533bdbe87670d48c7e193b2f1d6da25bdf2000
Karina Koldaeva <karina.mila@yandex.ru>
2024-07-07 22:39:42 +0300

    feat: add error boundary

---
+++ react-rss/src/components/ErrorBoundary.tsx
@@ -0,0 +1,46 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+import React from 'react';
+
+interface ErrorProps {
+  children: ReactNode;
+}
+
+interface ErrorState {
+  hasError: boolean;
+  error: Error | null;
+  errorInfo: ErrorInfo | null;
+}
+class ErrorBoundary extends Component<ErrorProps, ErrorState> {
+  constructor(props: ErrorProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error('Caught an error', error, errorInfo);
+    this.setState({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-page">
+          <h1>Something went wrong.</h1>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
